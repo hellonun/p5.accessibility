ca51804b5291c4018e3df283e877d8dcb991f472
mithru - desktop <mithru27@gmail.com>
2018-06-13 01:26:43 -0400

    for real this time

--- dist/p5.accessibility.js
+++ dist/p5.accessibility.js
@@ -1390,7 +1390,8 @@ Registry.register(ShapeEntity);
     const self = this;
     /* global BaseEntity */
     BaseEntity.call(self, shapeObject, arguments, canvasX, canvasY);
-    this.type = Interceptor.currentColor + ` text: `+String(arguments[0]).substring(0, 20);
+    this.type = String(arguments[0]).substring(0, 20) + `(` + Interceptor.currentColor + `)`;
+
     this.populate = function(shapeObject, arguments, canvasX, canvasY) {
         this.location = this.getLocation(shapeObject, arguments, canvasX, canvasY);
         this.coordLoc = this.canvasLocator(shapeObject, arguments, canvasX, canvasY);
--- src/entities/textEntity.js
+++ src/entities/textEntity.js
@@ -2,7 +2,8 @@ function TextEntity(Interceptor, shapeObject, arguments, canvasX, canvasY) {
     const self = this;
     /* global BaseEntity */
     BaseEntity.call(self, shapeObject, arguments, canvasX, canvasY);
-    this.type = Interceptor.currentColor + ` text: `+String(arguments[0]).substring(0, 20);
+    this.type = String(arguments[0]).substring(0, 20) + `(` + Interceptor.currentColor + `)`;
+
     this.populate = function(shapeObject, arguments, canvasX, canvasY) {
         this.location = this.getLocation(shapeObject, arguments, canvasX, canvasY);
         this.coordLoc = this.canvasLocator(shapeObject, arguments, canvasX, canvasY);
